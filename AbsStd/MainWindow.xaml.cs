433d43562e99c78b9cdfdbe0f8c588c05525f4c8
marwanmar123 <youcode098@gmail.com>
2021-07-13 21:03:06 +0100

    Add project files.

---
+++ AbsStd/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace AbsStd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection connection = new SqlConnection();
+        String connect = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            getcountdb();
+        }
+
+
+        private void getcountdb()
+        {
+
+
+            connection.ConnectionString = connect;
+            connection.Open();
+            string query = $"SELECT COUNT(*) FROM Apprenant";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+
+            // Executes the query, and returns the first column of the first row in the result set returned by the query. Additional columns or rows are ignored.
+            int numRecords = Convert.ToInt32(command.ExecuteScalar());
+
+            tajriba.Text = numRecords.ToString();
+            connection.Close();
+
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DG_Zone_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_appr_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                apprenant winapprenant = new apprenant();
+                winapprenant.Show();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex + "");
+            }
+
+        }
+
+        private void btn_scrt_Click(object sender, RoutedEventArgs e)
+        {
+            secretaire secretaire = new secretaire();
+            secretaire.Show();
+        }
+
+        private void btn_formt_Click(object sender, RoutedEventArgs e)
+        {
+            Formateur1 forma = new Formateur1();
+            forma.Show();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ AbsStd/apprenant.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace AbsStd
+{
+    /// <summary>
+    /// Interaction logic for apprenant.xaml
+    /// </summary>
+    public partial class apprenant : Window
+    {
+        private SqlConnection connection = new SqlConnection();
+        String connect = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+        int id_selected;
+        private StringBuilder errors;
+        private Regex validator;
+
+        public apprenant()
+        {
+            InitializeComponent();
+            loaddata();
+
+
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (AreFieldsValid())
+            {
+                connection.Close();
+                connection.ConnectionString = connect;
+                connection.Open();
+                string query = $"INSERT INTO Apprenant ( nom, prenom, email, mdp , phone ,  idFormation  ) values ('{input_Lname.Text}','{input_name.Text}','{input_mail.Text}','{input_mdp.Text}','{input_phone.Text}',{int.Parse(idform.Text)})";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                MessageBox.Show("records has been Added");
+                loaddata();
+            }
+        }
+
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Close();
+            connection.ConnectionString = connect;
+            connection.Open();
+
+            string query = $"DELETE FROM  Apprenant WHERE idApprenant = '{int.Parse(id_delete.Text)}'";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("records has been deleted");
+            loaddata();
+            connection.Close();
+
+
+
+
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (AreFieldsValid())
+            {
+                connection.Close();
+                connection.ConnectionString = connect;
+                connection.Open();
+                string query = $"UPDATE Apprenant SET prenom='{input_name.Text}' , n m='{input_Lname.Text}'  , email='{input_mail.Text}' , mdp='{input_mdp.Text}' , idFormation={int.Parse(idform.Text)}   WHERE idApprenant={Int32.Parse(id_edit.Text)}";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                loaddata();
+            }
+        }
+        private void loaddata()
+        {
+
+            connection.Close();
+            connection.ConnectionString = connect;
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Apprenant", connection);
+            command.ExecuteNonQuery();
+            SqlDataReader dr = command.ExecuteReader();
+            DG_Zone.ItemsSource = dr;
+
+
+
+        }
+
+        private bool AreFieldsValid()
+        {
+            errors = new StringBuilder();
+
+            //Validate First Name and Last Name
+            validator = new Regex(@"^([A-Z][a-z]+)(s[A-Z][a-z]+)*$");
+
+            if (!validator.Match(input_name.Text).Success)
+                errors.AppendLine("First name is not in the proper format.");
+
+            if (!validator.Match(input_Lname.Text).Success)
+                errors.AppendLine("Last name is not in the proper format.");
+
+            //Validate Age
+            validator = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+
+            if (!validator.IsMatch(input_mail.Text))
+                errors.AppendLine("Invalid Email.");
+
+            validator = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+
+            if (!validator.IsMatch(input_mdp.Text))
+                errors.AppendLine("Invalid Password.");
+
+
+            validator = new Regex(@"^[0-9]{10}");
+
+            if (!validator.IsMatch(input_phone.Text))
+                errors.AppendLine("Invalid phone.");
+
+            validator = new Regex(@"^[0-4]{1}$");
+
+            if (!validator.IsMatch(idform.Text))
+                errors.AppendLine("Invalid Formation ID.");
+
+
+            if (errors.ToString() == String.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(errors.ToString(), "Validation Failed");
+
+                return false;
+            }
+        }
+
+    }
+
+}
